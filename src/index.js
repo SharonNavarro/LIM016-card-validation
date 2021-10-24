7dc2cd1d636ad3a2bd70ccf38da9f9060eede220
SharonNavarro <shanmowrk@gmail.com>
2021-10-24 12:36:32 -0500

    Se agregó una nueva carpeta por la instalacion de NodeJs. Se realizo el validator, se modifico HTML y Jv (todavia en proceso)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import validator from './validator.js';
+console.log(validator.maskify(79786756476));
 
-console.log(validator);
+// declaramos las variables globales
+let name="";
+let numero=document.getElementById("numerodata").value;
+;
+
+
+/*
+function leerNombre(){ 
+  name=document.getElementById("nombre").value;
+  if(name==""){
+  alert("Ingresa tu nombre por favor");
+  return false;
+  }
+document.getElementById("introduccionUno").style.display="none";
+document.getElementById("elegirOpcion").style.display='block'; 
+document.getElementById("elige").innerHTML=name+", elige qué prenda te gustaria llevar.";
+}
+*/
+
+
+/*
+function inicio(){
+document.getElementById("introduccionUno").style.display="block";
+document.getElementById("elegirOpcion").style.display="none";
+document.getElementById("vestidoHina").style.display="none";
+document.getElementById("trajeAckerman").style.display="none";
+document.getElementById("kimonoDraken").style.display="none";
+}
+*/
+
+
+/*
+function opcion1(){
+document.getElementById("elegirOpcion").style.display='none';
+document.getElementById("vestidoHina").style.display="block";
+}
+*/
+
+
+/*
+function opcion2(){
+document.getElementById("elegirOpcion").style.display='none';
+document.getElementById("trajeAckerman").style.display="block";
+}
+*/
+
+
+/*
+function opcion3(){
+document.getElementById("elegirOpcion").style.display='none';
+document.getElementById("kimonoDraken").style.display="block";
+}
+*/
+
+
+/*
+function validarTarjeta(){
+  numero=document.getElementById("numerodata").value;
+  if(numero.length<16){    
+  alert("Ingresa tu numero de tarjeta correctamente, por favor. Son 16 dígitos.")
+  return true;}
+  if(numero.length>16){    
+  alert("Ingresa tu numero de tarjeta correctamente, por favor. Son 16 dígitos.")
+  return true;}
+}
+*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
-};
-
+//VALIDADOR
+isValid: function (numero)
+  {
+    //declaro las variables, lo convierto en array y lo pongo de reversa
+  let numero= document.getElementById("numerodata");
+  let creditCard=Array.from(numero).map(Number).reverse();
+  let sumaTotal=0;
+    //hago un for para pasar los indices
+  for (i=0; i<=creditCard.lenght; i++) 
+  {
+    //condiciono si los indices son pares y luego lo duplico
+    if ([i]%2!==0) 
+    {
+      let creditCardNumber = creditCard[i]*2;
+      //condiciono si el resultado es mayor a 9 y le resto 9
+      if (creditCardNumber > 9) 
+      {
+        creditCardNumber -= 9;
+      }
+    }
+    sumaTotal = creditCardNumber.reduce((total, value) => total + value, 0);
+  }
+   return sumaTotal%10==0;
+  },
+  
+  maskify: function (numero) 
+  {
+  //declaramos dos variables. La primera sera para el segmento que no se mostrará y el segundo sí. Utilizo slice para cortar la longitud que deseo      
+  let numero=document.getElementById(numerodata);
+  let numeroNoVisible = numero.slice(0, -4);
+  let numeroVisible = numero.slice(-4)
+  //consultar si se puede utilizar la expresion regular "replace(/[a-zA-Z]/g, '#')"
+  //se convierte a array y con la funcion map se creo un subArray con la function(digito) que recorrerá cada item
+ numeroNoVisible = numeroNoVisible.split('').map(function(digito) {
+    return digito = "#"})
+//se retorna a string
+  numeroNoVisible = numeroNoVisible.join('');
+//se retorna el valor final juntando las dos variables concatenandolo
+  return numeroNoVisible.concat(numeroVisible);
+},
+}
 export default validator;
