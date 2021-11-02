1eb253816c38fdac916d5e8020edd87421a2e455
SharonNavarro <shanmowrk@gmail.com>
2021-11-02 01:46:53 -0500

    Pyoyecto casi concluido, solo falta la mitad de CSS, pero pasaron los tests

--- src/index.js
+++ src/index.js
@@ -1,69 +1,116 @@
 import validator from './validator.js';
-console.log(validator.maskify(79786756476));
-
 // declaramos las variables globales
 let name="";
-let numero=document.getElementById("numerodata").value;
-;
+let numeroVestido;
+let numeroTraje;
 
-
-/*
-function leerNombre(){ 
+let botonVerificarNombre=  document.getElementById("botonVerificarNombre");
+botonVerificarNombre.addEventListener("click", leerNombre);
+function leerNombre() { 
   name=document.getElementById("nombre").value;
   if(name==""){
-  alert("Ingresa tu nombre por favor");
+  alert("Ingresa tu nombre, por favor.");
   return false;
   }
+document.getElementById("inicio").style.display="block"; 
 document.getElementById("introduccionUno").style.display="none";
-document.getElementById("elegirOpcion").style.display='block'; 
+document.getElementById("elegirOpcion").style.display="block"; 
 document.getElementById("elige").innerHTML=name+", elige qué prenda te gustaria llevar.";
 }
-*/
+ 
 
 
-/*
-function inicio(){
+let inicio= document.getElementById("inicio");
+inicio.addEventListener("click",()=>window.location.reload());
+inicio.addEventListener("click", volver);
+function volver(){
 document.getElementById("introduccionUno").style.display="block";
 document.getElementById("elegirOpcion").style.display="none";
 document.getElementById("vestidoHina").style.display="none";
 document.getElementById("trajeAckerman").style.display="none";
-document.getElementById("kimonoDraken").style.display="none";
 }
-*/
 
 
-/*
-function opcion1(){
+
+let opcion1= document.getElementById("btnVestido");
+opcion1.addEventListener("click", opcionUno);
+function opcionUno(){
 document.getElementById("elegirOpcion").style.display='none';
 document.getElementById("vestidoHina").style.display="block";
 }
-*/
 
 
-/*
-function opcion2(){
+
+let opcion2= document.getElementById("btnTraje");
+opcion2.addEventListener("click", opcionDos);
+function opcionDos(){
 document.getElementById("elegirOpcion").style.display='none';
 document.getElementById("trajeAckerman").style.display="block";
 }
-*/
 
 
-/*
-function opcion3(){
-document.getElementById("elegirOpcion").style.display='none';
-document.getElementById("kimonoDraken").style.display="block";
+
+let x=document.getElementById("numerodataVestido");
+  x.addEventListener("keyup", (e) => {
+  let valorInput = e.target.value;
+  x.value = valorInput.replace(/\s/g, "").replace(/\D/g, "");}
+  );
+
+
+
+let ingresarNumeroVestido= document.getElementById("ingresarNumeroVestido");
+ingresarNumeroVestido.addEventListener("click", btnIngresarV);
+function btnIngresarV(){
+  numeroVestido=x.value;
+  document.getElementById("cajaDeInfoVestido").style.display="none";
+  if(validator.isValid(numeroVestido)==true){
+    document.getElementById("resultadoPositivoV").style.display="block";
+    document.getElementById("msjPositivoV").innerHTML="¡Gracias por tu compra! tu número de tarjeta "+ validator.maskify(numeroVestido) + " es válida.";
+  } else {
+    document.getElementById("resultadoNegativoV").style.display="block";
+    document.getElementById("msjNegativoV").innerHTML="Lo sentimos, tu número de tarjeta "+ validator.maskify(numeroVestido) + " no es válida.";
+   }
 }
-*/
 
-
-/*
-function validarTarjeta(){
-  numero=document.getElementById("numerodata").value;
-  if(numero.length<16){    
-  alert("Ingresa tu numero de tarjeta correctamente, por favor. Son 16 dígitos.")
-  return true;}
-  if(numero.length>16){    
-  alert("Ingresa tu numero de tarjeta correctamente, por favor. Son 16 dígitos.")
-  return true;}
+let btnIntentarlo= document.getElementById("intentarVestido")
+btnIntentarlo.addEventListener("click", btnIntentarV)
+function btnIntentarV(){
+  document.getElementById("cajaDeInfoVestido").style.display="block";
+  document.getElementById("resultadoPositivoV").style.display="none";
+  document.getElementById("resultadoNegativoV").style.display="none";
 }
-*/
+
+
+
+let y=document.getElementById("numerodataTraje");
+  y.addEventListener("keyup", (e) => {
+  let valorInput = e.target.value;
+  y.value = valorInput.replace(/\s/g, "").replace(/\D/g, "");}
+  );
+
+let ingresarNumeroTraje= document.getElementById("ingresarNumeroTraje");
+ingresarNumeroTraje.addEventListener("click", btnIngresarT);
+function btnIngresarT(){
+  numeroTraje=y.value;
+  document.getElementById("cajaDeInfoTraje").style.display="none";
+  if(validator.isValid(numeroTraje)==true){
+    document.getElementById("resultadoPositivoT").style.display="block";
+    document.getElementById("msjPositivoT").innerHTML="¡Gracias por tu compra! tu número de tarjeta "+ validator.maskify(numeroTraje) + " es válida.";
+  } else {
+    document.getElementById("resultadoNegativoT").style.display="block";
+    document.getElementById("msjNegativoT").innerHTML="Lo sentimos, tu número de tarjeta "+ validator.maskify(numeroTraje) + " no es válida.";
+   }
+}
+
+let intentarTraje= document.getElementById("intentarTraje");
+intentarTraje.addEventListener("click", btnIntentarT);
+function btnIntentarT(){
+  document.getElementById("cajaDeInfoTraje").style.display="block";
+  document.getElementById("resultadoPositivoT").style.display="none";
+  document.getElementById("resultadoNegativoT").style.display="none";
+}
+
+
+
+ 
+
--- src/validator.js
+++ src/validator.js
@@ -1,43 +1,47 @@
-const validator = {
-//VALIDADOR
-isValid: function (numero)
-  {
+const validator =
+{
+  //VALIDADOR
+  isValid: function (numeroTarjeta) {
     //declaro las variables, lo convierto en array y lo pongo de reversa
-  let numero= document.getElementById("numerodata");
-  let creditCard=Array.from(numero).map(Number).reverse();
-  let sumaTotal=0;
+    let creditCard = Array.from(numeroTarjeta).map(Number).reverse();
+    //console.log(creditCard);
+    let sumaTotal = 0;
     //hago un for para pasar los indices
-  for (i=0; i<=creditCard.lenght; i++) 
-  {
-    //condiciono si los indices son pares y luego lo duplico
-    if ([i]%2!==0) 
-    {
-      let creditCardNumber = creditCard[i]*2;
-      //condiciono si el resultado es mayor a 9 y le resto 9
-      if (creditCardNumber > 9) 
-      {
-        creditCardNumber -= 9;
+    for (let i = 0; i < creditCard.length; i++) {
+      //condiciono si los indices NO son pares y luego lo duplico
+      if (i % 2 !== 0) {
+        creditCard[i] = creditCard[i] * 2;
+        //condiciono si el resultado es mayor o igual a 10 y le resto 9
+        if (creditCard[i] >= 10) {
+          creditCard[i] -= 9;
+        }
       }
+      //sumaTotal = creditCard.reduce((total, value) => total + value);
+      sumaTotal += creditCard[i]
     }
-    sumaTotal = creditCardNumber.reduce((total, value) => total + value, 0);
-  }
-   return sumaTotal%10==0;
+    //console.log(creditCard);
+    //console.log(sumaTotal);
+    return sumaTotal % 10 == 0;
   },
-  
-  maskify: function (numero) 
-  {
-  //declaramos dos variables. La primera sera para el segmento que no se mostrará y el segundo sí. Utilizo slice para cortar la longitud que deseo      
-  let numero=document.getElementById(numerodata);
-  let numeroNoVisible = numero.slice(0, -4);
-  let numeroVisible = numero.slice(-4)
-  //consultar si se puede utilizar la expresion regular "replace(/[a-zA-Z]/g, '#')"
-  //se convierte a array y con la funcion map se creo un subArray con la function(digito) que recorrerá cada item
- numeroNoVisible = numeroNoVisible.split('').map(function(digito) {
-    return digito = "#"})
-//se retorna a string
-  numeroNoVisible = numeroNoVisible.join('');
-//se retorna el valor final juntando las dos variables concatenandolo
-  return numeroNoVisible.concat(numeroVisible);
-},
+
+  maskify: function(numeroTarjeta) {
+//console.log(numeroTarjeta);
+    //declaramos dos variables. La primera sera para el segmento que no se mostrará y el segundo sí. Utilizo slice para cortar la longitud que deseo      
+    let numeroNoVisible = Array.from(numeroTarjeta).slice(0, -4);
+    //console.log(numeroNoVisible);
+    let numeroVisible = Array.from(numeroTarjeta).slice(-4); 
+    //console.log(numeroVisible);
+    //consultar si se puede utilizar la expresion regular "replace(/[a-zA-Z]/g, '#')"
+    //se convierte a array y con la funcion map se creo un subArray con la function(digito) que recorrerá cada item
+    numeroNoVisible = numeroNoVisible.map(function (digito) {
+      // eslint-disable-next-line no-unused-vars
+      return digito = "#";
+    })
+    //se retorna a string
+    numeroNoVisible = numeroNoVisible.join("");
+    //se retorna el valor final juntando las dos variables concatenandolo
+    //console.log(numeroNoVisible.concat(numeroVisible.join("")));
+    return numeroNoVisible.concat(numeroVisible.join(""));
+  }
 }
 export default validator;
