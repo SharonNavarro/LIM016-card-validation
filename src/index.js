946327cb289c581bdf5e6455f57822e63210e44c
SharonNavarro <shanmowrk@gmail.com>
2021-11-03 00:10:08 -0500

    Se agregó la funcion para no validar espacios vacion en el input donde se ingresa el numero de trajeta y avance del README.md

--- src/index.js
+++ src/index.js
@@ -56,17 +56,21 @@ let x=document.getElementById("numerodataVestido");
   x.value = valorInput.replace(/\s/g, "").replace(/\D/g, "");}
   );
 
-
-
 let ingresarNumeroVestido= document.getElementById("ingresarNumeroVestido");
 ingresarNumeroVestido.addEventListener("click", btnIngresarV);
 function btnIngresarV(){
   numeroVestido=x.value;
-  document.getElementById("cajaDeInfoVestido").style.display="none";
+  //document.getElementById("cajaDeInfoVestido").style.display="none";
+  if(numeroVestido==""){
+    alert("Ingresa los digitos de tu tarjeta, por favor.");
+    return true
+  }
   if(validator.isValid(numeroVestido)==true){
+    document.getElementById("cajaDeInfoVestido").style.display="none";
     document.getElementById("resultadoPositivoV").style.display="block";
     document.getElementById("msjPositivoV").innerHTML="¡Gracias por tu compra! tu número de tarjeta "+ validator.maskify(numeroVestido) + " es válida.";
   } else {
+    document.getElementById("cajaDeInfoVestido").style.display="none";
     document.getElementById("resultadoNegativoV").style.display="block";
     document.getElementById("msjNegativoV").innerHTML="Lo sentimos, tu número de tarjeta "+ validator.maskify(numeroVestido) + " no es válida.";
    }
@@ -92,11 +96,17 @@ let ingresarNumeroTraje= document.getElementById("ingresarNumeroTraje");
 ingresarNumeroTraje.addEventListener("click", btnIngresarT);
 function btnIngresarT(){
   numeroTraje=y.value;
-  document.getElementById("cajaDeInfoTraje").style.display="none";
+  //document.getElementById("cajaDeInfoTraje").style.display="none";
+  if(numeroTraje==""){
+    alert("Ingresa los digitos de tu tarjeta, por favor.");
+    return true
+  }
   if(validator.isValid(numeroTraje)==true){
+    document.getElementById("cajaDeInfoTraje").style.display="none";
     document.getElementById("resultadoPositivoT").style.display="block";
     document.getElementById("msjPositivoT").innerHTML="¡Gracias por tu compra! tu número de tarjeta "+ validator.maskify(numeroTraje) + " es válida.";
   } else {
+    document.getElementById("cajaDeInfoTraje").style.display="none";
     document.getElementById("resultadoNegativoT").style.display="block";
     document.getElementById("msjNegativoT").innerHTML="Lo sentimos, tu número de tarjeta "+ validator.maskify(numeroTraje) + " no es válida.";
    }
@@ -109,8 +119,4 @@ function btnIntentarT(){
   document.getElementById("resultadoPositivoT").style.display="none";
   document.getElementById("resultadoNegativoT").style.display="none";
 }
-
-
-
- 
 
